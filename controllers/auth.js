3e86afd22ebba62067b7c95922e5244332c48da7
Dae Dowdell <dow_dell2010@yahoo.com>
2025-07-22 15:10:43 -0400

    signin logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
+const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
-const User = require("../models/user.js");
+const User = require('../models/user.js');
 
 
 //Routes====================================
-router.get("/sign-up", (req, res) => {
-    res.render("auth/sign-up.ejs");
+router.get('/sign-up', (req, res) => {
+    res.render('auth/sign-up.ejs');
 });
 
-router.post("/sign-up", async (req, res) => {
+router.post('/sign-up', async (req, res) => {
     const userInDatabase = await User.findOne({ username: req.body.username });
     if (userInDatabase) {
-        return res.send("Username already taken.");
+        return res.send('Username already taken.');
     }
     if (req.body.password !== req.body.confirmPassword) {
-        return res.send("Password and Confirm Password must match");
+        return res.send('Password and Confirm Password must match');
     }
     const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     req.body.password = hashedPassword;
@@ -24,6 +24,28 @@ router.post("/sign-up", async (req, res) => {
     const user = await User.create(req.body);
     res.send(`Thanks for signing up ${user.username}`);
 });
+
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs');
+});
+
+router.post('/sign-in', async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send('Login failed. Please try again.');
+    }
+    const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password);
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+    }
+    req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+    };
+
+    res.redirect("/");
+});
+
 
 
 // ==========================================
--- server.js
+++ server.js
@@ -1,44 +1,55 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const morgan = require("morgan");
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
 
-const authController = require("./controllers/auth.js");
+const authController = require('./controllers/auth.js');
 
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
+const port = process.env.PORT ? process.env.PORT : '3000';
 
 
-//Connections===========================================================
+
+
+//Connections==================================================================
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
+mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-//============Midware====================================================
+//Midware======================================================================
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
-app.use("/auth", authController);
+app.use('/auth', authController);
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-//Routes below=====================================================================
+
+//Routes below==================================================================
 
 // GET /(home page)
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
 });
-
-
-
-
 
 //Routes above=====================================================================
 app.listen(port, () => {
